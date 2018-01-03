066c250c23ece66e4f7878da50023c3cf60143ee
anthonysof <antonis.sofras@gmail.com>
2018-01-03 13:45:45 +0200

    changed sql file

--- userdb.sql
+++ userdb.sql
@@ -3,10 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2018 at 11:20 PM
+-- Generation Time: Jan 03, 2018 at 12:44 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
-CREATE DATABASE IF NOT EXISTS `userdb`
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `userdb`
 --
+CREATE DATABASE IF NOT EXISTS `userdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `userdb`;
 
 -- --------------------------------------------------------
 
@@ -28,10 +30,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `kratiseis`
 --
 
-CREATE TABLE `kratiseis` (
-  `ekdromi` varchar(50) NOT NULL,
-  `anaxorisi` varchar(50) NOT NULL,
-  `afiksi` varchar(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `kratiseis` (
+  `ekdromi` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `anaxorisi` varchar(100) NOT NULL,
+  `epistrofi` varchar(100) NOT NULL,
   `tickets` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
   `cost` varchar(50) NOT NULL
@@ -43,12 +45,12 @@ CREATE TABLE `kratiseis` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `firstname` varchar(100) NOT NULL,
-  `surname` varchar(100) NOT NULL
+  `firstname` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `surname` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -56,14 +58,6 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`email`, `username`, `password`, `firstname`, `surname`) VALUES
-('natasof@gmail.com', 'natasof', '$2y$10$KeuCE4vr94JkFO8lzPaGpOXjv30JEI.xJm4jy3E3ab1XAHW452xlS', 'natali', 'sofra'),
-('kostas@gmail.com', 'kostasof', '$2y$10$9SCtiPFHxZ7gNZWS/bkZQeQN0NJBIhTAFpsdiIQ7vyCKCVitpbxaq', 'kostas', 'sofras'),
-('antreas@gmail.com', 'antremap', '$2y$10$dhWyWoDdqGmevMSl/mrDg.P1wA.SyDbXisX7i3Z88Hqiczkfwu4bW', 'antreas', 'mparkoulis'),
-('asd@dfad', 'lolaer_---$$$', '$2y$10$ZCUHssSCfVC7sjwrZeFPQeuI4yVO4Rn.xQ0NGdzrY3BD.FDOy0fPi', 'kakadodo', 'kakado sdas'),
-('giannissar@gmail.com', 'giannisar', '$2y$10$fbfA7rOuejSBDehHjW9KgeSOjF1oRjpH3bWHuhOzYLq1a4YfoxS12', 'giannis', 'saragas'),
-('antolol@poutso.com', 'antolol23', '$2y$10$PKHiETHRf307vkPqmOhEBORVPPqwt5QgDa7VZ5zCKgP6bbRjnQHdO', 'antonakis', 'lolakis'),
-('dimbal@gmail.com', 'dimbal', '$2y$10$ZcFF8HlCd4Pc1bp1gj/xiuLFyrMs0E0DFk165vDHlj6Rc0c8r6eKC', 'dimitris', 'mpalastouras'),
-('akako@gmail.com', 'akakos', '$2y$10$0jkx/PV1z0.002d0c2mNq.4RduWXjWcdS0bZRrrbO35N5u9roNV3e', 'akaks', 'akakoks'),
 ('kakas@gmail.com', 'abcd', '$2y$10$izKww5NVh/y8XVC2ZYxwY.CFWj.qUmwFC9CRbVQ/QP8/WBzbTzsH.', 'antar', 'kakas');
 COMMIT;
 
